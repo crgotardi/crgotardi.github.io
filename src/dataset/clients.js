eb7fadf750f0a55bd964be990a9b581ff338619c
Cristiano <crgotardi@gmail.com>
2023-10-19 20:04:04 +0100

    translate

--- src/dataset/clients.js
+++ src/dataset/clients.js
@@ -4,38 +4,35 @@ export default [
         logo: '/images/razzo.png',
         alt: 'Logo Razzo Tech',
         website: 'https://razzo.tech',
-        about: `A razzo é uma software house brasileira, situada em Rio Grande do Sul, 
-        focada em desenvolvimento e sustentação de soluções digitais inovadoras.`
+        about: `A razzo is a Brazilian software house, located in Rio Grande do Sul, focused on the development and support of innovative digital solutions.`
     },
     {
         name: 'T2S',
         logo: '/images/t2s.png',
         alt: 'Logo T2S',
         website: 'https://www.t2s.com.br',
-        about: `A T2S é uma empresa brasileira, localizada em Santos, São Paulo. 
-        Oferece soluções de software com agilidade, excelência e exclusividade para o setor portuário, atendendo portos de todo o país.`
+        about: `Located in Santos, São Paulo, T2S offers software solutions with agility, excellence, and exclusivity to the port sector, serving ports throughout the country.`
     },
     {
         name: 'Leadlovers',
         logo: '/images/leadlovers.png',
         alt: 'Logo Leadlovers',
         website: 'https://leadlovers.com/en',
-        about: `A leadlovers é uma empresa brasileira, referência em automação de marketing digital. 
-        Tem como objetivo simplificar tarefas e otimizar resultados, ideal para atrair novos contatos para o seu negócio e transformá-los em clientes.`
+        about: `Leadlovers is a Brazilian company that is a leader in digital marketing automation. It aims to simplify tasks and optimize results, making it ideal for attracting new leads to your business and converting them into customers.`
     },
     {
         name: 'Aubay',
         logo: '/images/aubay.png',
         alt: 'Logo Aubay',
         website: 'https://aubay.com',
-        about: `A Aubay Portugal é uma multinacional de origem francesa, em Portugal desde 2007. 
-        Com sede em Lisboa e escritórios no Porto, é uma consultora especializada na gestão, implementação, desenvolvimento e manutenção de Sistemas de Informação.`
+        about: `Aubay Portugal is a French multinational company that has been operating in Portugal since 2007.
+        Headquartered in Lisbon and with offices in Porto, it is a consultancy specialized in the management, implementation, development and maintenance of Information Systems.`
     },
     {
         name: 'SL Benfica',
         logo: '/images/benfica.png',
         alt: 'Logo SL Benfica',
         website: 'https://www.slbenfica.pt',
-        about: `O Sport Lisboa e Benfica é um dos maiores clubes multidesportivos português, campeão nacional de futebol em 38 temporadas e venceu a Liga dos Campeões 2 vezes.`
+        about: `Sport Lisboa e Benfica is one of the biggest multi-sport clubs in Portuguese, national football champion in 38 seasons and has won the Champions League 2 times.`
     },
 ]
